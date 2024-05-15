7039280ca63e5d451b80f50a8a8ced3892757523
Lo-Lap <116972772+Lo-Lap@users.noreply.github.com>
2024-05-15 21:29:16 +0300

    Edit run.py and grade.json

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from GameField import GameField
 from task import Optimizer
 import json
+import time
 
 FIELD_FILENAME = "field.csv"
 
@@ -8,41 +9,44 @@ FIELD_FILENAME = "field.csv"
 def main():
     field = GameField()
     field.fill(FIELD_FILENAME)
-    best_chromosome = Optimizer.optimize(field)
-    result = field.testAnt(best_chromosome)
-    max_answer = field.get_max_answer()
-    print(f"Your result is {result}")
-    print(f"Max is {max_answer}")
-    grade = 0
-    percent = result * 100 / max_answer
+    TIMEOUT_SECONDS = 100
 
-    if percent >= 90:
-        grade = 5
-    elif percent >= 85:
-        grade = 4
-    elif percent >= 80:
-        grade = 3
-    elif percent >= 65:
-        grade = 2
-    elif percent >= 55:
-        grade = 1
+    try:
+        start_time = time.time()
+        best_chromosome = Optimizer.optimize(field)
+        result = field.testAnt(best_chromosome)
+        print(f"Your result is {result}")
+        print("Max is 89")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(elapsed_time)
+        # if elapsed_time > TIMEOUT_SECONDS:
+        #     raise Exception("Test exceeded time limit.")
+        max_answer = field.get_max_answer()
+        grade = 0
 
-    # if 50 <= result < 60:
-    #     grade = 1
-    # elif 80 > result >= 60:
-    #     grade = 2
-    # elif 82 > result >= 80:
-    #     grade = 3
-    # elif 84 > result >= 82:
-    #     grade = 4
-    # elif result >= 84:
-    #     grade = 5
+        percent = result * 100 / max_answer
+
+        if percent >= 90:
+            grade = 5
+        elif percent >= 85:
+            grade = 4
+        elif percent >= 80:
+            grade = 3
+        elif percent >= 65:
+            grade = 2
+        elif percent >= 55:
+            grade = 1
+
+    except Exception as e:
+        print(f"Error during optimization: {e}")
+        grade = e
 
     with open('grade.json') as fp:
         dictObj = json.load(fp)
 
     with open('grade.json', 'r+') as file:
-        dictObj['grade'].append(grade)
+        dictObj['grade'] = grade
         file.write(json.dumps(dictObj))
         file.close()
 
